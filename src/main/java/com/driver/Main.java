b4e46cbe22a2dd0588d2c957afba15f2aab063b6
diptendu <mandaldiptendu99@gmail.com>
2023-02-08 02:31:55 +0530

    Second Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,18 +1,21 @@
 package com.driver;
 
-class A{
-    public String meth(){
-        return "Invoking method from class A";
-    }
-}
-class B extends A{
-    public String meth(){
-        return "Method is overridden in Extendend class B";
-    }
-}
 public class Main {
+    public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A{
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
     public static void main(String[] args){
-        A b = new B();
+        B a = new B();
+        a.meth();
+
+        B b = new B();
         b.meth();
     }
 }
